d61023207088e9d622d338eb3921a8954d3a3af3
Traveler-Yang <1187095757@qq.com>
2025-04-20 08:07:47 +0800

    code

---
+++ Src/Client/Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityScript.Scripting.Pipeline;
+
+public class LoadingManager : MonoBehaviour
+{
+
+	public GameObject UITips;//提示图片
+	public GameObject UILoadingPanel;//加载场景
+	public GameObject UILoaginPanel;//登录场景
+	public GameObject UIRegisterPanel;//注册场景
+
+	public Slider loadingSlider;//加载进度条
+	public Text loadingNumberText;//加载显示数字
+	public Text loagingText;//加载显示文本
+
+	private IEnumerator WaitLoading()
+	{
+        UITips.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+		//UILoadingPanel.SetActive(true);
+		UITips.SetActive(false);
+
+        for (float i = 30; i <= 100;)
+        {
+			i += Random.Range(1f, 1.5f);
+            loadingSlider.value = i;
+            loadingNumberText.text = loadingSlider.value.ToString();
+			yield return new WaitForEndOfFrame();
+            if (loadingSlider.value >= 100)
+            {
+                UILoadingPanel.SetActive(false);
+                UILoaginPanel.SetActive(true);
+			}
+        }
+
+    }
+	
+	void Start ()
+	{
+		StartCoroutine(WaitLoading());
+		loadingSlider.value = 0;
+	}
+	
+	
+	void Update ()
+	{
+		
+	}
+}
---
+++ Src/Client/Assets/Scripts/UI/UIRegisters.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Services;
+
+public class UIRegisters : MonoBehaviour {
+
+	public InputField username;//账号
+	public InputField password;//密码
+	public InputField verifyPassword;//确认密码
+	public Button registersButton;//注册按钮
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+    }
+
+    public void OnRegistersClickButton()
+    {
+		if (string.IsNullOrEmpty(username.text))
+		{
+            MessageBox.Show("请输入账号");
+            return;
+		}
+        if (string.IsNullOrEmpty(password.text))
+        {
+            MessageBox.Show("请输入密码");
+            return;
+        }
+        if (string.IsNullOrEmpty(verifyPassword.text))
+        {
+            MessageBox.Show("请输入确认密码");
+            return;
+        }
+        if (password.text != verifyPassword.text)
+        {
+            MessageBox.Show("两次输入的密码不一致");
+            return;
+        }
+        UserService.Instance.SendRegister(username.text, password.text);
+    }
+}
