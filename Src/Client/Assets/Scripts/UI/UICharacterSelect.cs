b107235215bc8746f138d59c7f34e15961f5439e
Traveler-Yang <1187095757@qq.com>
2025-04-24 06:59:36 +0800

    更新

---
+++ Src/Client/Assets/Scripts/UI/UICharacterSelect.cs
@@ -0,0 +1,81 @@
+﻿using Models;
+using Services;
+using SkillBridge.Message;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICharacterSelect : MonoBehaviour {
+
+    public GameObject playerFoundPanel;//选择角色界面
+    public GameObject characterSelectPanel;//创建角色界面
+
+    CharacterClass charClass;
+
+    public Text descs;//职业描述
+
+    public Text[] names;//职业名字
+
+    public Text characterName;//角色昵称
+
+    public UICharacterView characterView;
+
+    void Start () {
+        UserService.Instance.OnCharacterCreate = OnCharacterCreate;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void InitCharacterSelect(bool init)
+    {
+        playerFoundPanel.SetActive(true);
+        characterSelectPanel.SetActive(false);
+        if (init)
+        {
+
+        }
+    }
+
+    public void OnCharacterCreate()
+    {
+        if (string.IsNullOrEmpty(characterName.text))
+        {
+            MessageBox.Show("请输入昵称！");
+            return;
+        }
+        UserService.Instance.SendCharacterCreate(this.characterName.text, charClass);
+    }
+
+    void OnCharacterCreate(Result result, string message)
+    {
+        if (result == Result.Success)
+        {
+            InitCharacterSelect(true);
+        }
+        else
+        {
+            MessageBox.Show(message, "错误 {0}", MessageBoxType.Error);
+        }
+    }
+
+    public void OnSelectClass(int charClass)
+    {
+        this.charClass = (CharacterClass)charClass;
+
+        characterView.CurrectCharacter = charClass - 1;
+
+        for (int i = 0; i < 3; i++)
+        {
+            names[i].text = DataManager.Instance.Characters[i + 1].Name;//将职业名字依次赋值给UI
+        }
+
+        descs.text = DataManager.Instance.Characters[charClass].Description;//将职业描述赋值到UI
+
+    }
+}
---
+++ Src/Client/Assets/Scripts/UI/UICharacterView.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharacterView : MonoBehaviour {
+
+	public GameObject[] characters;
+
+	private int currectCharacter = 0;
+	public int CurrectCharacter 
+	{
+		get
+		{
+			return currectCharacter;
+		}
+		set
+		{
+			currectCharacter = value;
+			UpdateCharacter();
+
+        }
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void UpdateCharacter()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			characters[i].SetActive(i == currectCharacter);
+		}
+	}
+}
