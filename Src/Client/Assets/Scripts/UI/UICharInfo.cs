e349e06a914010ceb66b5b25f7941369a214d170
Traveler-Yang <1187095757@qq.com>
2025-04-28 20:28:25 +0800

    AddCode

---
+++ Src/Client/Assets/Scripts/UI/UICharInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICharInfo : MonoBehaviour {
+
+	public SkillBridge.Message.NCharacterInfo info;
+
+	public Text charclass;//职业
+	public Text charName;//昵称
+	public Image highlight;//高亮图片
+
+	public bool Selected//是否被选中
+	{
+		get { return highlight.IsActive(); }
+		set
+		{
+			highlight.gameObject.SetActive(value);
+		}
+	}
+
+	void Start () {
+		if (info != null)
+        {
+            this.charclass.text = info.Class.ToString();
+            this.charName.text = info.Name;
+        }
+	}
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Src/Server/GameServer/GameServer/Managers/CharacterManager.cs
@@ -0,0 +1,58 @@
+﻿using Common;
+using GameServer.Entities;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Managers
+{
+    class CharacterManager : Singleton<CharacterManager>
+    {
+        public Dictionary<int, Character> Characters = new Dictionary<int, Character>();
+
+        public CharacterManager()
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Init()
+        {
+
+        }
+        /// <summary>
+        /// 清空角色字典
+        /// </summary>
+        public void Clear()
+        {
+            Characters.Clear();
+        }
+        /// <summary>
+        /// 添加角色到字典
+        /// </summary>
+        /// <param name="cha"></param>
+        /// <returns></returns>
+        public Character AddCharacter(TCharacter cha)
+        {
+            Character character = new Character(CharacterType.Player, cha);
+            this.Characters[cha.ID] = character;
+            return character;
+        }
+        /// <summary>
+        /// 移除
+        /// </summary>
+        /// <param name="characterId"></param>
+        public void RemoveCharacter(int characterId)
+        {
+            this.Characters.Remove(characterId);
+        }
+    }
+}
